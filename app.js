f6a93ce484be5e94156abbbf1dd8bf2bc65c2139
TheMaestro0 <abdovc012@gmail.com>
2021-01-18 10:34:56 +0200

    Add bot mention as optional prefix.

--- app.js
+++ app.js
@@ -20,16 +20,22 @@ const client = new Client({
 client.start();
 
 
-
+const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 const prefix = client.prefix;
 const cooldowns = client.cooldowns;
 
 client.on("message", async message => {
 	if (message.author.bot || !message.guild) return;
-	if (message.content.indexOf(prefix) !== 0) return;
+
+	const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
+	
+	if (!prefixRegex.test(message.content)) return;
+
+	const [, matchedPrefix] = message.content.match(prefixRegex);
+
 	if (!message.channel.permissionsFor(client.user).has("SEND_MESSAGES")) return;
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
 
 	const command = client.commands.get(commandName) || client.commands.get(client.aliases.get(commandName));
